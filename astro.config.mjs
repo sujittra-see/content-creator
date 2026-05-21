fad5e89d651f7a8faf2c7161d4c62435877c2864
sujittra-see <sujittra.laa@gmail.com>
2026-05-21 23:11:19 +0700

    6th

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
